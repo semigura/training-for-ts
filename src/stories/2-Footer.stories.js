9e032e318196bd0c5140ab3a78ad7e3d6f53ef79
semigura <shotaro-ishigaki@gaji.jp>
2020-02-14 12:57:41 +0900

    Fix app

---
+++ src/stories/2-Footer.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Footer } from './../components/_Footer'
+
+export default {
+  title: 'Footer',
+  component: Footer,
+};
+
+export const Text = () => <Footer />;
